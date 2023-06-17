20754240759ef0d4c083c38b9d8aa3196d07e13b
Aman Kumar Singh <79279299+Jr-Einstein@users.noreply.github.com>
2023-06-18 01:07:59 +0530

    Added Javascript file.

---
+++ script.js
@@ -0,0 +1,10 @@
+
+const password = prompt("Enter Your Password to Access the Website : ")
+
+const num = 1234;
+if(password == num){
+    console.log("Logined Succesfully!!");
+}else{
+    alert("Incorrect Password! , Login Again");
+    
+}
